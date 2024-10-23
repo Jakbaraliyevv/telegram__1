2197aa228206e7b12a83d109f03c3f292a9f4af1
AzizJakbaraliyev <azizjakbaraliyev454@gamil.com>
2024-10-23 17:23:43 +0500

    rasm yuklasa boladi

--- main.js
+++ main.js
@@ -1,73 +1,3 @@
-// const sarvar__api = "https://6715fa1733bc2bfe40bbca78.mockapi.io/Sarvarbek";
-// const me__api = `https://6715fa1733bc2bfe40bbca78.mockapi.io/Azizakam`;
-
-// const input__text__For__sarvar = document.getElementById(
-//   "input__text__For__sarvar"
-// );
-
-// const send__btn__for__sarvar = document.getElementById(
-//   "send__btn__for__sarvar"
-// );
-
-// const massage__div = document.querySelector(".massage__div");
-// BU ozgaruvchilar tepadagilar
-
-// function post__Sarvar() {
-//   fetch(sarvar__api, {
-//     method: "POST",
-//     headers: { "Content-Type": "application/json" },
-//     body: JSON.stringify({
-//       text: input__text__For__sarvar.value,
-//     }),
-//   })
-//     .then((data) => data.json())
-//     .then((data) => console.log(data))
-
-//     .catch((error) => console.log(error.massage));
-// }
-
-// send__btn__for__sarvar.addEventListener("click", (e) => {
-//   e.preventDefault();
-
-//   if (input__text__For__sarvar.value !== "") {
-//     post__Sarvar();
-//     input__text__For__sarvar.value = "";
-//   }
-// });
-
-// // Bu sarvarga yozgan ozimni xabarim
-
-// fetch(me__api)
-//   .then((data) => data.json())
-//   .then((value) => get__sarvar__text(value))
-//   .catch((error) => console.log(error.massage));
-
-// function get__sarvar__text(value) {
-//   value.forEach((element) => {
-//     massage__div.innerHTML += `
-//      <div class="massage__text">
-//               <p>${element.text}</p>
-//             </div>
-//       `;
-//   });
-// }
-
-// fetch(sarvar__api)
-//   .then((data) => data.json())
-//   .then((value) => get__me__text(value))
-//   .catch((error) => console.log(error.massage));
-
-// function get__me__text(value) {
-//   value.forEach((element) => {
-//     massage__div.innerHTML += `
-//       <div class="massage__textme">
-//               <p>${element.text}</p>
-//               <img src="./img/check.svg" alt="" />
-//             </div>
-//       `;
-//   });
-// }
-
 // Ikkinchi variyandi
 
 const input__text__For__sarvar = document.getElementById(
@@ -82,6 +12,14 @@ const massage__div = document.querySelector(".massage__div");
 
 const apii = "https://6718988a7fc4c5ff8f4a1f17.mockapi.io/users";
 
+function date() {
+  let date = new Date();
+  let hour = date.getHours() > 10 ? 0 + date.getHours() : date.getHours();
+  let minute =
+    date.getMinutes() > 10 ? 0 + date.getMinutes() : date.getMinutes();
+  return `${hour}:${minute}`;
+}
+
 function post__Sarvar() {
   fetch(apii, {
     method: "POST",
@@ -89,6 +27,7 @@ function post__Sarvar() {
     body: JSON.stringify({
       text: input__text__For__sarvar.value,
       ID: 1,
+      date: date(),
     }),
   })
     .then((data) => data.json())
@@ -106,28 +45,107 @@ send__btn__for__sarvar.addEventListener("click", (e) => {
   }
 });
 
-// // Bu sarvarga yozgan ozimni xabarim
-
 fetch(apii)
   .then((data) => data.json())
   .then((value) => get__me__text(value))
-  .catch((error) => console.log(error.massage));
+  .catch((error) => console.log(error.message)); // error.massage emas, error.message
 
 function get__me__text(value) {
   value.forEach((element) => {
     if (element.ID === 1) {
       massage__div.innerHTML += `
-      <div class="massage__textme">
-              <p>${element.text}</p>
-              <img src="./img/check.svg" alt="" />
-            </div>
+        <div class="massage__textme">
+          <p>${element.text}</p>
+          <div class="data__time">
+            <span>${element.date}</span>
+            <img src="./img/check.svg" alt="" />
+          </div>
+        </div>
       `;
     } else if (element.ID === 2) {
       massage__div.innerHTML += `
-      <div class="massage__text">
-               <p>${element.text}</p>
-             </div>
-       `;
+        <div class="massage__text">
+          <img src="${element.image_user}" alt="Yuklangan rasm" style="max-width: 200px; border-radius: 10px;">
+          <p>${element.text}</p>
+          <div class="data__time">
+            <span>${element.date}</span>
+          </div>
+        </div>
+      `;
+    }
+  });
+}
+
+// Uje yangi narsalaga otyappiz rasm yuklash endi
+const rasm = document.getElementById("rasm");
+const fileInput = document.getElementById("fileInput");
+
+rasm.addEventListener("click", () => {
+  fileInput.click();
+});
+
+// // psot img bu
+
+fileInput.addEventListener("change", (e) => {
+  const file = e.target.files[0];
+  if (file) {
+    const reader = new FileReader();
+    reader.onload = (event) => {
+      const imgData = event.target.result;
+
+      fetch("https://6718988a7fc4c5ff8f4a1f17.mockapi.io/send_img", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({
+          image_user: imgData,
+          ID: 1,
+          date: date(),
+        }),
+      })
+        .then((response) => {
+          return response.json(); // Agar serverdan JSON formatida javob kelsa
+        })
+        .then((data) => {
+          console.log(data);
+        })
+        .catch((error) => {
+          console.error(error.message);
+        });
+    };
+
+    reader.readAsDataURL(file);
+  } else {
+    console.error("Fayl tanlanmadi!");
+  }
+});
+
+// Get img uchun
+
+fetch("https://6718988a7fc4c5ff8f4a1f17.mockapi.io/send_img").then((data) =>
+  data.json().then((value) => get__img(value))
+);
+function get__img(value) {
+  value.forEach((element) => {
+    if (element.ID === 1) {
+      massage__div.innerHTML += `
+<div class="massage__textme">
+  <img src="${element.image_user}" alt="Yuklangan rasm" style="max-width: 200px; border-radius: 10px;">
+  <div class="data__time">
+    <span>${element.date}</span>
+    <img src="./img/check.svg" alt="" />
+
+  </div>
+</div>
+`;
+    } else if (element.ID === 2) {
+      massage__div.innerHTML += `
+<div class="massage__textme">
+  <img src="${element.image_user}" alt="Yuklangan rasm" style="max-width: 200px; border-radius: 10px;">
+  <div class="data__time">
+    <span>${element.date}</span>
+  </div>
+</div>
+`;
     }
   });
 }
